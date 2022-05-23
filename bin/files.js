9131c2f8bde44609ec5ccac89ce557f6820fbc1f
billiboba1 <shadrinalexander@mail.ru>
2022-05-23 19:58:57 +0300

    //3

--- bin/files.js
+++ bin/files.js
@@ -12,44 +12,44 @@ const sortFn = (x, y) => {
   return 0;
 }
 
-export const genDiff = (filepath1, filepath2) => {
-  const generateDifference = (file1, file2) => {
-    let resultString = '{\n';
-    const obj = {};
-    const generateResultList = (child) => {
-      if ((deleteList.length > 0) && (deleteList[deleteList.length - 1][0] === child[0]) && (deleteList[deleteList.length - 1][1] === child[1])) {
-        deleteList.pop();
-        resultString += `    ${child[0]}: ${child[1]}\n`;
-      } else {
-        if (array1.includes(child) && !array2.includes(child)) {
-          resultString += `  - ${child[0]}: ${child[1]}\n`;
-        } else if (!array1.includes(child) && array2.includes(child)) {
-          resultString += `  + ${child[0]}: ${child[1]}\n`;
-        }
+const generateDifference = (file1, file2) => {
+  let resultString = '{\n';
+  const generateResultString = (child) => {
+    if ((deleteList.length > 0) && (deleteList[deleteList.length - 1][0] === child[0]) && (deleteList[deleteList.length - 1][1] === child[1])) {
+      deleteList.pop();
+      resultString += `    ${child[0]}: ${child[1]}\n`;
+    } else {
+      if (array1.includes(child) && !array2.includes(child)) {
+        resultString += `  - ${child[0]}: ${child[1]}\n`;
+      } else if (!array1.includes(child) && array2.includes(child)) {
+        resultString += `  + ${child[0]}: ${child[1]}\n`;
       }
     }
-    file1 = JSON.parse(file1);
-    file2 = JSON.parse(file2);
-    const array1 = Object.entries(file1);
-    const array2 = Object.entries(file2);
-    const overallArray = [...array1, ...array2];
-    overallArray.sort(sortFn);
-    const newArray = [];
-    const deleteList = [];
-    for (let i = 0; i < overallArray.length - 1; i += 1) {
-      if (!((overallArray[i][0] === overallArray[i + 1][0]) && (overallArray[i][1] === overallArray[i + 1][1]))) {
-        newArray.push(overallArray[i]);
-      } else {
-        deleteList.splice(0, 0, overallArray[i]);
-      }
-    }
-    newArray.push(overallArray[overallArray.length - 1]);
-    newArray.map(generateResultList);
-    return resultString + '}';
   }
+  file1 = JSON.parse(file1);
+  file2 = JSON.parse(file2);
+  const array1 = Object.entries(file1);
+  const array2 = Object.entries(file2);
+  const overallArray = [...array1, ...array2];
+  overallArray.sort(sortFn);
+  const newArray = [];
+  const deleteList = [];
+  for (let i = 0; i < overallArray.length - 1; i += 1) {
+    if (!((overallArray[i][0] === overallArray[i + 1][0]) && (overallArray[i][1] === overallArray[i + 1][1]))) {
+      newArray.push(overallArray[i]);
+    } else {
+      deleteList.splice(0, 0, overallArray[i]);
+    }
+  }
+  newArray.push(overallArray[overallArray.length - 1]);
+  newArray.map(generateResultString);
+  console.log(resultString + '}');
+  return resultString + '}';
+}
 
-  const path1 = path.resolve(filepath1);
-  const path2 = path.resolve(filepath2);
+export const genDiff = (filepathes) => {
+  const path1 = path.resolve(filepathes[0]);
+  const path2 = path.resolve(filepathes[1]);
   const file1 = fs.readFileSync(path1, "utf8");
   const file2 = fs.readFileSync(path2, "utf8");
 
--- bin/gendiff.js
+++ bin/gendiff.js
@@ -9,12 +9,14 @@ program
   .description('Usage: gendiff [options] <filepath1> <filepath2>' +
       '\n\nCompares two configuration files and shows a difference.')
   .option('-f, --format <type>', 'output format')
+  .arguments('<files...>')
+  .action(genDiff)
   .parse(process.argv);
+
 
 const { args } = program;
 const options = program.opts();
 if (options.format) {
-} else {
-  console.log(genDiff(args[0], args[1]));
+  
 }
 
